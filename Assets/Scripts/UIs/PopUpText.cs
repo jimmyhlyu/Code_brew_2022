2109a72db679be20fc08d27033245fe27d72d6da
JimmyLyu <jimmylyu616@gmail.com>
2022-04-21 23:39:32 +1000

    UI Pop up Text

---
+++ Assets/Scripts/UIs/PopUpText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class PopUpText
+{
+    private static GameObject textPosition1;
+
+    public static GameObject TextPosition1
+    {
+        get => textPosition1;
+        set => textPosition1 = value;
+    }
+
+    public static void Pop(string msg)
+    {
+        var text = textPosition1.GetComponent<TMP_Text>();
+
+        text.text = msg;
+          
+        
+      
+        
+    }
+}

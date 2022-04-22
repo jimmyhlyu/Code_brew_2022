f728ea884d7372d79973613ec09ffc1b6ef70faa
JimmyLyu <jimmylyu616@gmail.com>
2022-04-22 16:21:41 +1000

    Typer writer

--- Assets/Scripts/UIs/PopUpTextAuthoring.cs
+++ Assets/Scripts/UIs/PopUpTextAuthoring.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Security.Cryptography.X509Certificates;
 using TMPro;
 using UnityEngine;
 using Unity.UI;
@@ -15,14 +15,81 @@ public class PopUpTextAuthoring : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject textPosition1;
-
+    public PauseInfo pauseInfo;
     public GameObject TextPosition1 => textPosition1;
-
-    private string outputLetters;
+    private string acturalString = "";
+    private string finalString;
+    private int index; 
+    
     private void OnEnable()
     {
         PopUpText.TextPosition1 = textPosition1;
     }
-    
 
+    private void Start()
+    {
+        PopText("hello world");
+    }
+
+    private void PopText(string String)
+    {
+        finalString = String;
+        ReproduceText();
+    }
+
+    private void ReproduceText()
+    {
+        //if not readied all letters
+        if (index < finalString.Length)
+        {
+            //get one letter
+            char letter = finalString[index];
+
+            //Actualize on screen
+            PopUpText.Pop(Write(letter));
+
+            //set to go to the next
+            index += 1;
+            StartCoroutine(PauseBetweenChars(letter));
+        }
+    }
+
+    private string Write(char letter)
+    {
+        acturalString += letter;
+        return acturalString;
+    }
+
+    private IEnumerator PauseBetweenChars(char letter)
+    {
+        switch (letter)
+        {
+            case '.':
+                yield return new WaitForSeconds(pauseInfo.dotPause);
+                ReproduceText();
+                yield break;
+            case ',':
+                yield return new WaitForSeconds(pauseInfo.commaPause);
+                ReproduceText();
+                yield break;
+            case ' ':
+                yield return new WaitForSeconds(pauseInfo.spacePause);
+                ReproduceText();
+                yield break;
+            default:
+                yield return new WaitForSeconds(pauseInfo.normalPause);
+                ReproduceText();
+                yield break;
+        }
+    }
 }
+
+[Serializable]
+public class PauseInfo
+{
+    public float dotPause;
+    public float commaPause;
+    public float spacePause;
+    public float normalPause;
+}
+
